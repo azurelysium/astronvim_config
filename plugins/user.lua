c7bf9826e8e438ce392407a6dc4fed935e38f701
Minhwan Kim <azurelysium@gmail.com>
2023-10-12 14:45:38 +0900

    Add todo-comments library

--- plugins/user.lua
+++ plugins/user.lua
@@ -25,4 +25,13 @@ return {
       })
     end,
   },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    pts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  },
 }
