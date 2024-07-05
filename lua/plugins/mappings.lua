4fa670a814f9ee50d46aca968dc26cf4909f84e3
Minhwan Kim <azurelysium@gmail.com>
2024-07-05 17:00:35 +0900

    Add spectre

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -40,6 +40,9 @@ return {
           ["<leader>O"] = { ":Neotree reveal<cr>" },
           ["<leader><Space>"] = { ":CopilotChatToggle<cr>" },
 
+          ["<leader>ss"] = { "<cmd>lua require('spectre').toggle()<CR>", desc = "Toggle Spectre" },
+          ["<leader>sw"] = { "<cmd>lua require('spectre').open_visual({select_word=true})<CR>", desc = "Search current word" },
+          ["<leader>sp"] = { "<cmd>lua require('spectre').open_file_search({select_word=true})<CR>", desc = "Search on current file" },
         },
         t = {
           -- setting a mapping to false will disable it
@@ -60,6 +63,7 @@ return {
             end,
           },
           ["<leader><Space>"] = { ":CopilotChat " },
+          ["<leader>sw"] = { "<cmd>lua require('spectre').open_visual({select_word=true})<CR>", desc = "Search current word" },
         },
       },
     },
---
+++ lua/plugins/spectre.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "nvim-pack/nvim-spectre",
+    lazy = false,
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+    },
+    opts = {
+    }
+  },
+}
